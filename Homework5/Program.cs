7422736d29bce3405bedf39f06d763149d95dd9c
Lesha07 <simchuklesha@gmail.com>
2024-10-24 12:39:01 +0300

    1

--- Homework5/Program.cs
+++ Homework5/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("input your number");
+try
+{
+    byte number = byte.Parse(Console.ReadLine());
+    
+    if(number >= 0 && number <= 255)
+    {
+        Console.WriteLine("Correct number");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("incorrect number format");
+}
+finally
+{
+    Console.WriteLine("Goodbye!");
+}       
